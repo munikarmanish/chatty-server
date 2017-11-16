ec1876163e589e88e5785adee0725bcf1a1cd0f2
Manish Munikar <munikarmanish@gmail.com>
2017-11-16 10:21:31 +0545

    initial commit graphql works

---
+++ data/schema.js
@@ -0,0 +1,10 @@
+const Schema = [
+  `type Query {
+    testString: String
+  }
+  schema {
+    query: Query
+  }`,
+];
+
+export default Schema;
---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import Schema from './data/schema';
+import Mocks from './data/mocks';
+
+const GRAPHQL_PORT = 8080;
+const app = express();
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+});
+
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true,
+});
+
+// `context` must be an object
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema: executableSchema,
+  context: {}, // at least(!) an empty object
+}));
+
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
+}));
+
+const graphQLServer = http.createServer(app);
+
+graphQLServer.listen(GRAPHQL_PORT, () => {
+  console.log(`GraphQL http://localhost:${GRAPHQL_PORT}/graphql`);
+});
